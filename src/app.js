9e4a6b24fe2e4fa5a712dc7750c6973f0190333f
francoChocobar <118401544+francoChocobar@users.noreply.github.com>
2023-08-14 21:32:57 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import path from "path";
+import { __dirname } from "./utils.js";
+import { productsRouter } from "./routes/products.routes.js";
+import { cartsRouter } from "./routes/carts.routes.js";
+import { viewsRouter } from "./routes/views.routes.js";
+
+const port = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => console.log("server listening on port 8080"));
+
+
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use(viewsRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+export const config = {
+    server: {
+        port:8080
+    },
+    fileSystem:{
+        productsFile: "products.json",
+        cartFile:"carts.json"
+    },
+    mongo:{
+        url: "mongodb+srv://proyectoBackEnd:franco_chocobar41969274@cluster0.l2ryfyv.mongodb.net/tienda43380?retryWrites=true&w=majority"
+    }
+}
